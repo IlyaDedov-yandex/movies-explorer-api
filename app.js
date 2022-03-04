e947b4ed15af051509eaf22f59caba1fb7708cf7
Ilya Dedov <ilya-tor92@yandex.ru>
2022-03-05 01:39:06 +1000

    Диплом. Настройка cors 5

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
@@ -17,11 +16,6 @@ const { PORT = 4000 } = process.env;
 const app = express();
 app.use(requestLogger);
 app.use(limiter);
-app.use(cors({
-  origin: ['https://movies.practicum.nomoredomains.work/', /\.movies.practicum.nomoredomains.work$/, 'http://movies.practicum.nomoredomains.work/'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
