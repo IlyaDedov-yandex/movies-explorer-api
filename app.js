7f2266e63b2c1f5c4ef1482a8ed4e968a43f643a
Ilya Dedov <ilya-tor92@yandex.ru>
2022-03-05 01:42:57 +1000

    Диплом. Настройка cors 6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const checkErrors = require('./middlewares/check-errors');
 const { limiter } = require('./middlewares/rate-limits');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(requestLogger);
